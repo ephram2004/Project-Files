1516f439c9ff4e19284c8da3c37705632e02d0d4
Ephram Cola Jacquin <ecolajacquin@gmail.com>
2022-12-13 19:39:01 -0500

    Add MainMenu() Function

--- v1.0.py
+++ v1.0.py
@@ -33,3 +33,37 @@ def StartGame():
 
 
 def MainMenu():
+    """
+    Directs the program to the proper functions based on user-requested inputs. Outputs
+    random letter and asks user to roll again or begin the game.
+
+    Parameters
+    ------------------
+    none
+
+    Returns
+    ------------------
+    none
+    """
+
+    letterToPlay = 'A'                                      #initialize variables
+    usrOption = 0
+
+    while usrOption is not 3:                               #asks for user option input
+        letterToPlay = RandLetter(letterToPlay)             #calls function to output random letter
+        print(f'\nCurrent Letter is: {letterToPlay}\n')
+        usrOption = int(input(f"Select an option:\n1. Start game\n2. Re-Roll Letter\n3. Exit\n"))
+        match usrOption:
+            case 1:
+                StartGame()
+            case 2:
+                letterToPlay = RandLetter(letterToPlay)
+            case 3:
+                break
+            case _:
+                print("Invalid Option. Please Try Again.")
+
+
+
+
+MainMenu()
